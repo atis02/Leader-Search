2c4437033bd8f4ffc8f3ad9ca2bb2ed05d66389e
atis02 <guwancikramow@gmail.com>
2023-12-18 02:11:25 +0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Leader-Search/',
   plugins: [react()],
 })
-// export default defineConfig({
-// plugins: [react()],
-// })
